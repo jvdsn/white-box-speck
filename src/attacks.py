c6ede6fa7c507a9c9efdcebeea50c1d19ff8abfa
Joachim Vandersmissen <git@jvdsn.com>
2021-08-29 22:18:28 +0200

    Update linear attack with ring.

--- src/attacks.py
+++ src/attacks.py
@@ -77,29 +77,29 @@ def attack_linear_encodings(block_size, key_size, matrices, vectors):
     key_words = key_size // word_size
     key = [0] * key_words
     wb = WhiteBoxSpeck(block_size, key_size, key)
-    self_equivalence_provider = LinearSelfEquivalenceProvider(gf2, word_size)
+    self_equivalence_provider = LinearSelfEquivalenceProvider(word_size)
 
     rotate_x_right = wb._rotate_right_matrix(wb.alpha, 0)
     rotate_y_left = wb._rotate_left_matrix(0, wb.beta)
     xor_xy = wb._xor_xy_matrix()
 
     c = recover_coefficients_linear_1(word_size, wb.alpha, wb.beta, matrices[2])
-    O, _, _, _ = self_equivalence_provider.self_equivalence(c)
+    O, _, _, _ = self_equivalence_provider.self_equivalence(gf2, c)
     input_external_encoding = (O * rotate_x_right * xor_xy * rotate_y_left) ** -1 * matrices[1]
     input_external_encoding = (input_external_encoding, vector(gf2, 2 * word_size))
 
     c = recover_coefficients_linear_1(word_size, wb.alpha, wb.beta, matrices[wb.rounds - 1])
-    _, _, I, _ = self_equivalence_provider.self_equivalence(c)
+    _, _, I, _ = self_equivalence_provider.self_equivalence(gf2, c)
     O = matrices[wb.rounds - 1] * (rotate_x_right * xor_xy * rotate_y_left * I) ** -1
     c = recover_coefficients_linear_2(word_size, O)
-    _, _, I, _ = self_equivalence_provider.self_equivalence(c)
+    _, _, I, _ = self_equivalence_provider.self_equivalence(gf2, c)
     output_external_encoding = matrices[wb.rounds] * (xor_xy * rotate_y_left * I) ** -1
     output_external_encoding = (output_external_encoding, vector(gf2, 2 * word_size))
 
     k = []
     for i in range(key_words):
         c = recover_coefficients_linear_1(word_size, wb.alpha, wb.beta, matrices[i + 2])
-        O, _, _, _ = self_equivalence_provider.self_equivalence(c)
+        O, _, _, _ = self_equivalence_provider.self_equivalence(gf2, c)
         v = (O * rotate_x_right * xor_xy * rotate_y_left) ** -1 * vectors[i + 1]
         k.append(0)
         for j in range(word_size):
@@ -120,7 +120,7 @@ def test_attack_linear_encodings(block_size, key_size):
     wb = WhiteBoxSpeck(block_size, key_size, key)
     input_external_encoding = external_encodings.random_linear_external_encoding(word_size)
     output_external_encoding = external_encodings.random_linear_external_encoding(word_size)
-    matrices, vectors = wb.affine_layers(input_external_encoding, output_external_encoding, LinearSelfEquivalenceProvider(gf2, word_size))
+    matrices, vectors = wb.affine_layers(input_external_encoding, output_external_encoding, LinearSelfEquivalenceProvider(word_size))
     key_, input_external_encoding_, output_external_encoding_ = attack_linear_encodings(block_size, key_size, matrices, vectors)
     print("Recovered key?", key_ == key)
     print("Recovered input external encoding?", input_external_encoding_ == input_external_encoding)
